1320dc75343739be74c1197cb8b54f522521b3d6
PeterYang1625 <peteryang1625@gmail.com>
2015-11-13 23:32:16 -0800

    Store Progress
    Most of this current code is layout example. It will later be replaced
    with php magic that auto fills in products from a database

--- contact/index.php
+++ contact/index.php
@@ -32,7 +32,7 @@
             <div class="col-6">
                 <p>Thanks for your interest in Block Party! If you have any additional questions about Block Party specifically, you can reach us using the contact form on this page, or by e-mailing us at <a href="mailto:contact@veblockparty.com">contact@veblockparty.com</a>.</p><br>
 			     <p>If you would like to reach us through mail, you can write to us, Block Party, at <a href="https://www.google.com/maps/place/11125+Knott+Ave,+Cypress,+CA+90630/@33.8268232,-118.0361785,15z/data=!4m2!3m1!1s0x80dd29291591741b:0x814e6f997e29e1d5">11125 Knott Ave, Cypress, CA 90630*</a>.</p>
-                <img id="contact-image" src="/img/placeholder.png">
+                <img class="hide-on-mobile" src="/img/placeholder.png">
             </div>
             <div class="col-6 contact-form">
                 <form action="contact.php" method="POST">
